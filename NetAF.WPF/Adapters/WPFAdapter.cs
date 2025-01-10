2bf7dc4086c939c8a362b0b36ade81f35cee18e2
ben_pollard <ben@singerinstruments.com>
2025-01-10 16:35:58 +0000

    Adjusted adpater

---
+++ NetAF.WPF/Adapters/WPFAdapter.cs
@@ -0,0 +1,50 @@
+﻿using NetAF.Logic;
+using NetAF.Rendering;
+
+namespace NetAF.Adapters
+{
+    /// <summary>
+    /// Provides an adapter for the WPF.
+    /// </summary>
+    public sealed class WPFAdapter : IIOAdapter
+    {
+        #region Implementation of IIOAdapter
+
+        /// <summary>
+        /// Setup for a game.
+        /// </summary>
+        /// <param name="game">The game to set up for.</param>
+        public void Setup(Game game)
+        {
+        }
+
+        /// <summary>
+        /// Render a frame.
+        /// </summary>
+        /// <param name="frame">The frame to render.</param>
+        public void RenderFrame(IFrame frame)
+        {
+            frame.Render(presenter);
+        }
+
+        /// <summary>
+        /// Wait for acknowledgment.
+        /// </summary>
+        /// <returns>True if the acknowledgment was received correctly, else false.</returns>
+        public bool WaitForAcknowledge()
+        {
+            
+        }
+
+        /// <summary>
+        /// Wait for input.
+        /// </summary>
+        /// <returns>The input.</returns>
+        public string WaitForInput()
+        {
+            
+        }
+
+        #endregion
+    }
+}
